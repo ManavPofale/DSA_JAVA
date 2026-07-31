bfc0866b5b8864e9ad209ad82e77c0150629f8ba
Manav Bharat Pofale <manavpofale@gmail.com>
2026-07-31 12:54:01 +0530

    Time: 10 ms (66.76%), Space: 48 MB (73.7%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -8,7 +8,6 @@ class Solution {
         int keypress = 0;
         int count = 0;
         for(int i=25;i>=0;i--){
-            if(arr[i]==0) break;
             int val=arr[i]*(count/8 + 1);
             keypress+=val;
             count++;
