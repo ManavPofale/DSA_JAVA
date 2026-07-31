62b52cf732eb758b63b5442e14f2703988941f75
Manav Bharat Pofale <manavpofale@gmail.com>
2026-07-31 11:46:42 +0530

    Time: 0 ms (100%), Space: 43.1 MB (66.43%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,9 +1,28 @@
+// class Solution {
+//     public int minimumPushes(String word) {
+//         int ans = 0;
+//         for(int i=0;i<word.length();i++){
+//             ans += (i/8) + 1;
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int minimumPushes(String word) {
-        int ans = 0;
-        for(int i=0;i<word.length();i++){
-            ans += (i/8) + 1;
+        int n = word.length();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (i < 8){
+                sum += 1;
+            }else if (i < 16){
+                sum += 2;
+            }else if (i < 24){
+                sum += 3;
+            }else{
+                sum+= 4;
+            }    
         }
-        return ans;
+        return sum;
     }
 }
