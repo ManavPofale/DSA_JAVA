dffd98cf0017ea5246589f0427dd93ce2b09f03b
Manav Bharat Pofale <manavpofale@gmail.com>
2026-07-31 12:43:57 +0530

    Time: 10 ms (66.76%), Space: 48.4 MB (17.34%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int arr[] = new int[26];
+        for(char ch : word.toCharArray()){
+            arr[ch - 'a']++;
+        }
+        Arrays.sort(arr);
+        int keypress = 0;
+        int count = 0;
+        for(int i=25;i>=0;i--){
+            int mul = 0;
+            if(count<8){
+                mul=1;
+            }else if(count<16){
+                mul=2;
+            }else if(count<24){
+                mul=3;
+            }else{
+                mul=4;
+            }
+            int val=arr[i]*mul;
+            keypress+=val;
+            count++;
+        }
+        return keypress;
+    }
+}
