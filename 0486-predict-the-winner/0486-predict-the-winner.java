12ccadff7cbc659c3ba5d128e99db2b5749b322e
Manav Bharat Pofale <manavpofale@gmail.com>
2026-08-01 22:41:54 +0530

    Time: 1 ms (51.87%), Space: 42.7 MB (57.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,18 +1,51 @@
+// class Solution {
+
+//     public boolean predictTheWinner(int[] nums) {
+//         return solve(nums, 0, nums.length - 1) >= 0;
+//     }
+
+//     private int solve(int[] nums, int left, int right) {
+
+//         if (left == right)
+//             return nums[left];
+
+//         int takeLeft = nums[left] - solve(nums, left + 1, right);
+
+//         int takeRight = nums[right] - solve(nums, left, right - 1);
+
+//         return Math.max(takeLeft, takeRight);
+//     }
+// }
+
+
+// optimal solution using DP
+
 class Solution {
 
     public boolean predictTheWinner(int[] nums) {
-        return solve(nums, 0, nums.length - 1) >= 0;
-    }
 
-    private int solve(int[] nums, int left, int right) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
 
-        if (left == right)
-            return nums[left];
+        // Base case
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
 
-        int takeLeft = nums[left] - solve(nums, left + 1, right);
+        for (int len = 2; len <= n; len++) {
 
-        int takeRight = nums[right] - solve(nums, left, right - 1);
+            for (int i = 0; i + len - 1 < n; i++) {
 
-        return Math.max(takeLeft, takeRight);
+                int j = i + len - 1;
+
+                int takeLeft = nums[i] - dp[i + 1][j];
+
+                int takeRight = nums[j] - dp[i][j - 1];
+
+                dp[i][j] = Math.max(takeLeft, takeRight);
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
     }
 }
