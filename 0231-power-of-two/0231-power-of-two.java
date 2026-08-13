fd15f4d9e0ea723c029eb13c228437a5aff8dd0f
Manav Bharat Pofale <manavpofale@gmail.com>
2026-08-13 21:23:46 +0530

    Time: 1 ms (96.59%), Space: 42.6 MB (37.01%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        // for(int i=0;i*i<=n+1;i++){
+        //     if(Math.pow(2,i)==n){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        if(n==0) return false;
+        while(n%2==0){
+            n/=2;
+        }
+        return n==1;
+    }
+}
