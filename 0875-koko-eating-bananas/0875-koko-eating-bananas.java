11337708789ff982835d8edb38783aa80f146e49
Manav Bharat Pofale <manavpofale@gmail.com>
2026-08-15 21:42:28 +0530

    Time: 9 ms (56.68%), Space: 47.9 MB (45.17%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,54 +1,25 @@
-// class Solution {
-//     public int minEatingSpeed(int[] piles, int h) {
-//         int left = 1;
-//         int right = 0;
-//         for (int pile : piles) {
-//             right = Math.max(right, pile);
-//         }
-//         int ans = right;
-//         while (left <= right) {
-//             int mid = left + (right - left) / 2;
-//             long hours = 0;
-//             for (int pile : piles) {
-//                 hours += (pile + mid - 1) / mid;   
-//             }
-//             if (hours <= h) {
-//                 ans = mid;
-//                 right = mid - 1;
-//             } else {
-//                 left = mid + 1;
-//             }
-//         }
-//         return ans;
-//     }
-// }
-
-
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int l=1, r=0;
-        for (int pile:piles){
-            r=Math.max(r, pile);
+        int l = 1;
+        int r = 0;
+        for(int pile:piles){
+            r=Math.max(r,pile);
         }
-        while(l<r){
+        while(l<=r){
             int mid=l+(r-l)/2;
-            if(isValid(piles, mid, h)){
-                r=mid;
+            long hours=0;
+            for(int pile:piles){
+                hours+=(pile+(long) mid-1)/mid;
+                if(hours>h){
+                    break;
+                }
+            }
+            if(hours<=h){
+                r = mid-1;
             }else{
                 l=mid+1;
             }
         }
         return l;
     }
-    public boolean isValid(int[] piles, int mid, int h){
-        int ch=0;
-        for(int i=0;i<piles.length;i++){
-            if(piles[i]%mid==0 ){
-                ch+=piles[i]/mid;
-            }else{
-                ch+=(piles[i]/mid)+1;
-            }
-        }
-        return ch<=h;
-    }
-}    
+}
