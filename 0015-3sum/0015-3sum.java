981aa762d4341fdc358aeb2dee43d81e8f651847
Manav Bharat Pofale <manavpofale@gmail.com>
2026-08-18 09:18:42 +0530

    Time: 34 ms (60.11%), Space: 59.3 MB (45.89%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,11 +1,9 @@
 class Solution {
     public List<List<Integer>> threeSum(int nums[]){
-
         List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
-
         Arrays.sort(nums);
-
+        
         for(int i=0;i<n-2;i++){
             if(i>0 && nums[i]==nums[i-1])
                 continue;
