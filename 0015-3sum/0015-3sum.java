bca67f431ccc00549684def2ff5059b73b8ca3fa
Manav Bharat Pofale <manavpofale@gmail.com>
2026-08-18 09:29:36 +0530

    Time: 420 ms (12.64%), Space: 59.6 MB (21.11%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,68 @@
+// class Solution {
+//     public List<List<Integer>> threeSum(int nums[]){
+//         List<List<Integer>> ans = new ArrayList<>();
+//         int n = nums.length;
+//         Arrays.sort(nums);
+        
+//         for(int i=0;i<n-2;i++){
+//             if(i>0 && nums[i]==nums[i-1])
+//                 continue;
+//             int left = i+1;
+//             int right = n-1;
+
+//             while(left<right){
+//                 int sum = nums[i]+nums[left]+nums[right];
+//                 if(sum == 0){
+//                     ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+//                     left++;
+//                     right--;
+//                     while(left<right && nums[left] == nums[left-1]){
+//                         left++;
+//                     }
+//                     while(left<right && nums[right] == nums[right+1]){
+//                         right--;
+//                     }
+//                 }else if(sum<0){
+//                     left++;
+//                 }else{
+//                     right--;
+//                 }
+//             }
+//         }    
+//         return ans;
+//     }
+// }
+
+// using hashset
 class Solution {
-    public List<List<Integer>> threeSum(int nums[]){
+    public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
+        
         Arrays.sort(nums);
         
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i]==nums[i-1])
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
-            int left = i+1;
-            int right = n-1;
-
-            while(left<right){
-                int sum = nums[i]+nums[left]+nums[right];
-                if(sum == 0){
-                    ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
-                    left++;
-                    right--;
-                    while(left<right && nums[left] == nums[left-1]){
-                        left++;
-                    }
-                    while(left<right && nums[right] == nums[right+1]){
-                        right--;
-                    }
-                }else if(sum<0){
-                    left++;
-                }else{
-                    right--;
-                }
             }
-        }    
+            
+            Set<Integer> seen = new HashSet<>();
+            
+            for (int j = i + 1; j < n; j++) {
+                int complement = -nums[i] - nums[j];
+                
+                if (seen.contains(complement)) {
+                    ans.add(Arrays.asList(nums[i], complement, nums[j]));
+                    
+                    while (j + 1 < n && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                }
+                
+                seen.add(nums[j]);
+            }
+        }
+        
         return ans;
     }
 }
