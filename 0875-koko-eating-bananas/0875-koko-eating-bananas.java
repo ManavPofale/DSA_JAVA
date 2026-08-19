7d563d1748b93322121fae4aef823e70e37b078b
Manav Bharat Pofale <manavpofale@gmail.com>
2026-08-19 09:04:45 +0530

    Time: 21 ms (29.13%), Space: 48.4 MB (16.21%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,62 +1,25 @@
-// class Solution {
-//     public int minEatingSpeed(int[] piles, int h) {
-//         int l = 1;
-//         int r = 0;
-//         for(int pile:piles){
-//             r=Math.max(r,pile);
-//         }
-//         while(l<=r){
-//             int mid=l+(r-l)/2;
-//             long hours=0;
-//             for(int pile:piles){
-//                 // hours+=(pile+(long) mid-1)/mid;
-//                 if(pile%mid==0){
-//                     hours+=pile/mid;
-//                 }else{
-//                     hours+=(pile/mid)+1;
-//                 }
-//                 if(hours>h){
-//                     break;
-//                 }
-//             }
-//             if(hours<=h){
-//                 r = mid-1;
-//             }else{
-//                 l=mid+1;
-//             }
-//         }
-//         return l;
-//     }
-// }
-
-
-// using method
-
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int l = 1;
-        int r = 0;
-        for (int pile : piles) {
-            r = Math.max(r, pile);
+        int minspeed = 1;
+        int maxspeed = 0;
+        for(int pile : piles){
+            maxspeed = Math.max(pile, maxspeed);
         }
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (canFinish(piles, h, mid)) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+        while(minspeed<maxspeed){
+            int mid = minspeed + (maxspeed - minspeed) / 2;
+            if(caneatintime(piles, h, mid)){
+                maxspeed = mid;
+            }else{
+                minspeed = mid + 1;
             }
         }
-        return l;
+        return minspeed;
     }
-    private boolean canFinish(int[] piles, int h, int speed) {
+    public boolean caneatintime(int[] piles, int h, int mid){
         long hours = 0;
-        for (int pile : piles) {
-            hours += (pile + (long) speed - 1) / speed;
-            if (hours > h) {
-                return false;
-            }
+        for(int pile : piles){
+            hours += (int)Math.ceil((double)pile/mid);
         }
-        return true;
+        return hours<=h;
     }
 }
