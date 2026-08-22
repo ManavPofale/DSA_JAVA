5741c870f378acc15fe21f4200a517f1de4be6eb
Manav Bharat Pofale <manavpofale@gmail.com>
2026-08-22 10:50:05 +0530

    Time: 4 ms (10.14%), Space: 268.2 MB (5.51%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,13 +1,36 @@
+// brute force solution
+// class Solution {
+//     public void rotate(int[] nums, int k) {
+//         int n = nums.length;
+//         k = k % n;
+
+//         for(int i = 0; i < k; i++){
+//             int temp = nums[n - 1];
+//             for(int j = n - 1; j > 0; j--){
+//                 nums[j] = nums[j - 1];
+//             }
+//             nums[0] = temp;
+//         }
+//     }
+// }
+
+// optimal solution
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k % n;
-        int[] temp = new int[n];
-        for(int i = 0; i < n; i++){
-            temp[(i + k) % n] = nums[i];
-        }
-        for(int i = 0; i < n; i++){
-            nums[i] = temp[i];
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+    private void reverse(int[] nums, int left, int right){
+        while(left < right){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
         }
     }
 }
