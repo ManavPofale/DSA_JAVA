85d13aff3d92475857d729b0d011beb5de67f9cb
Manav Bharat Pofale <manavpofale@gmail.com>
2026-08-22 11:07:00 +0530

    Time: 2 ms (99.33%), Space: 47.4 MB (11.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,50 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int n = nums.length;
+//         int[][] pairs = new int[n][2];
+//         // Store value and original index
+//         for(int i = 0; i < n; i++){
+//             pairs[i][0] = nums[i];
+//             pairs[i][1] = i;
+//         }
+//         // Sort based on the values
+//         Arrays.sort(pairs, (a, b) -> Integer.compare(a[0], b[0]));
+        
+//         int left = 0;
+//         int right = n - 1;
+//         while(left < right){
+//             int sum = pairs[left][0] + pairs[right][0];
+            
+//             if(sum == target){
+//                 return new int[] { pairs[left][1], pairs[right][1] };
+//             }else if(sum < target){
+//                 left++;
+//             }else{
+//                 right--;
+//             }
+//         }
+//         return new int[] {};
+//     }
+// }
+
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i,j};
-                }
-            }
+        int[] ans = new int[2];
+
+        if(nums.length == 0 || nums.length < 2){
+            return ans ;
         }
-        return new int[]{};
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            int left = target - nums[i];
+            if(map.containsKey(left)){
+                return new int[] {map.get(left),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{0,0};        
     }
 }
