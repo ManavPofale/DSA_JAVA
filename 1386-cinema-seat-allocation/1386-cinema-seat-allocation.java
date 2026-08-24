e30b83aebfc1594580b4e8cae76570a98e9d4e53
Manav Bharat Pofale <manavpofale@gmail.com>
2026-08-24 10:50:54 +0530

    Time: 19 ms (60.1%), Space: 52.5 MB (61.13%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int[] seat : reservedSeats) {
+            int row = seat[0];
+            int col = seat[1];
+            if (col >= 2 && col <= 9) {
+                map.put(row, map.getOrDefault(row, 0) | (1 << (col - 2)));
+            }
+        }
+
+        int ans = (n - map.size()) * 2;
+
+        for (int mask : map.values()) {
+            boolean left = (mask & 0b00001111) == 0;
+            boolean right = (mask & 0b11110000) == 0;
+            boolean middle = (mask & 0b00111100) == 0;
+
+            if (left && right) {
+                ans += 2;
+            } else if (left || right || middle) {
+                ans += 1;
+            }
+        }
+
+        return ans;
+    }
+}
