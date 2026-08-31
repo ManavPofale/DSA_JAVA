2fea5712eba44911094220695672bf856bb87f2b
Manav Bharat Pofale <manavpofale@gmail.com>
2026-08-31 18:54:50 +0530

    Time: 3 ms (73.93%), Space: 69.5 MB (62.99%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,8 +1,23 @@
+// class Solution {
+//     public double findMaxAverage(int[] nums, int k) {
+//         int sum=0;
+//         for(int left=0;left<k;left++){
+//             sum+=nums[left];
+//         }
+//         int maxsum=sum;
+//         for(int i=k;i<nums.length;i++){
+//             sum+=nums[i]-nums[i-k];
+//             maxsum=Math.max(maxsum, sum);
+//         }
+//         return (double)maxsum/k;
+//     }
+// }
+
 class Solution {
-    public double findMaxAverage(int[] nums, int k) {
+    public double findMaxAverage(int[] nums, int k){
         int sum=0;
-        for(int left=0;left<k;left++){
-            sum+=nums[left];
+        for(int right=0;right<k;right++){
+            sum+=nums[right];
         }
         int maxsum=sum;
         for(int i=k;i<nums.length;i++){
@@ -11,4 +26,4 @@ class Solution {
         }
         return (double)maxsum/k;
     }
-}
+}    
