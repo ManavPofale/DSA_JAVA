f36de4c9304cca9ec629dc68c99122021aa32102
Manav Bharat Pofale <manavpofale@gmail.com>
2026-09-06 09:19:17 +0530

    Time: 13 ms (93.74%), Space: 42.7 MB (95.03%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -3,10 +3,9 @@ class Solution {
         int m = t.length();
         long[] dp = new long[m + 1];
         dp[0] = 1;
-
-        for (int i = 1; i <= s.length(); i++) {
-            for (int j = m; j >= 1; j--) {
-                if (s.charAt(i - 1) == t.charAt(j - 1)){
+        for(int i=1;i<=s.length();i++){
+            for(int j=m;j>=1;j--){
+                if(s.charAt(i-1) == t.charAt(j-1)){
                     dp[j] += dp[j - 1];
                 }
             }
